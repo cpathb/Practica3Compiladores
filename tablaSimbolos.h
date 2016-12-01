f549e16f6506970da452215187268b7c15a3a525
Carlos Parada Alfaya <carlos.p.a123@gmail.com>
2016-12-01 22:29:30 +0100

    Delete tablaSimbolos.h

--- tablaSimbolos.h
+++
@@ -1,9 +0,0 @@
-#include "tabla_hash_encadenamiento.h"
-
-#define tamLinea 100
-#pragma once
-
-void inicializarTablasimbolos();
-void destruirTablasimbolos();
-int obtenerCompLex(char * lexema);
-void ImprimirTablaSimbolos();

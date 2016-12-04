30904651b6318f2c96f4e01cc0673089f14e89ca
Carlos Parada Alfaya <carlos.p.a123@gmail.com>
2016-12-04 21:19:21 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 int main( int argc, const char* argv[] ){
     inicializarTablaSimbolos(); // inicializamos la "tabla de simbolos"
     yyin = stdin; // Definimos la entrada de flex como la entrada del sistema
-    printf("Iniciando \"Complex Calculator\"...\n\Recuerde que puede obtener ayuda usando el comando \"help\"o \"HELP\"\n\n\n");
+    printf("Iniciando \"Complex Calculator\"...\nRecuerde que puede obtener ayuda usando el comando \"help\"o \"HELP\"\n\n\n");
     while(1){
         yyparse(); // Lanzamos la ejecución del analizador sintáctico
     }
